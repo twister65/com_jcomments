8a29abb9a6b1acab4d60608ef3f76b7102e3a727
twister65 <olaverre@free.fr>
2018-09-21 05:35:46 +0200

    Modify JComments Update Site.

--- site/tpl/JoomSpirit_theme/tpl_form.php
+++ site/tpl/JoomSpirit_theme/tpl_form.php
@@ -129,9 +129,10 @@ class jtt_tpl_form extends JoomlaTuneTemplate
 		}
 
 		if ($this->getVar('comments-form-captcha', 0) == 1) {
-			$html = $this->getVar('comments-form-captcha-html','kcaptcha');
+			$html = $this->getVar('comments-form-captcha-html');
 			if ($html == 'kcaptcha') {
 				$link = JCommentsFactory::getLink('captcha');
+
 ?>
 <p>
 	<span>
@@ -142,7 +143,7 @@ class jtt_tpl_form extends JoomlaTuneTemplate
 </p>
 <?php
 			} else {
-				$recaptcha = JCaptcha::getInstance('recaptcha', array('namespace' => 'anything'));
+				$recaptcha = JCaptcha::getInstance($html, array('namespace' => 'anything'));
 				echo $recaptcha->display('recaptcha', 'dynamic_recaptcha_1','g-recaptcha');
 ?>
 <?php
